ca5b094f5752048483431c0ca9bd0c0dc79aa880
Bailey Whitbread <98004245+baileywhitbread@users.noreply.github.com>
2024-04-16 21:53:10 +1000

    Update paper.jl
    Added helper functions and isolated pseudo Levis

--- paper.jl
+++ paper.jl
@@ -6,35 +6,61 @@ function orderpol(G)
 	return CycPol(PermRoot.generic_order(G,Pol(:q)))
 end
 
+function pi0(L)
+	return length(algebraic_center(L).AZ)
+end
+
+function subset(L,M)
+	return issubset(inclusion(L),inclusion(M))
+end
+
+function mob(A,B,poset)
+	if A == B
+		return 1
+	elseif subset(A,B)
+		mobvalue = 0
+		for element in poset
+			if subset(A,element) && subset(element,B) && element != B
+				mobvalue += mob(A,element,poset)
+			end
+		end
+		return (-1)*mobvalue
+	else
+		error("First argument must be less than the second argument")
+	end
+end
+
+function nu(L)
+	nuvalue = 0
+	for iplevi in iplevis
+		if subset(L,iplevi)
+			nuvalue += mob(L,iplevi,plevis)*pi0(iplevi)
+		end
+	end
+	return nuvalue
+end
 
 
-# Choose group G. Some options are: 
-# gl, pgl, so, psp, G2, F4, E6, E7, E8 
-G=rootdatum(:so,5)
+# Choose group G
+G = rootdatum(:so,5);
 
 # Gather info about G
-Gdual = rootdatum(simplecoroots(G),simpleroots(G))
-rank = size(simpleroots(G),2)
-ssrank = size(simpleroots(G),1)
-W = rootdatum(cartan(G))
-QIR = quasi_isolated_reps(G)
-UC = UnipotentCharacters(G)
-Z = algebraic_center(G)[1]
-T = torus(G,position_class(G,G(1,2))).W 
+Gdual = rootdatum(simplecoroots(G),simpleroots(G));
+rank = size(simpleroots(G),2);
+ssrank = size(simpleroots(G),1);
+W = rootdatum(cartan(G));
+orderT = orderpol(torus(rank));
+orderZ = orderpol(torus(rank-ssrank));
 
-# Compute order polynomials
-orderG = orderpol(G)
-orderT = orderpol(T)
-orderZ = orderpol(torus(rank-ssrank))
-
-# Compute pseudo levis in Gdual
+# Compute pseudo Levis and isolated pseudo Levis in Gdual
 plevis = reflection_subgroup.(Ref(Gdual),sscentralizer_reps(Gdual)); 
-# For a one-argument function f, we have f.([a b c]) = [f(a) f(b) f(c)]
-# For a two-argument function g, we have g.(Ref(x),[a b c]) = [g(x,a) g(x,b) g(x,c)]
+iplevis = unique(map(L -> L.group, centralizer.(Ref(Gdual),quasi_isolated_reps(Gdual))));
+# Without unique(), iplevis has repeated elements, eg. when G=SO5
 
-# Compute levis in Gdual
+
+
+# Compute Levis in Gdual
 levis = map(L -> L.W, split_levis(Gdual)); 
-# split_levis(Gdual) is a list of spets objects, L -> L.W makes them groups
 
 # Compute number of G-types
 number_of_gptypes = 0;
@@ -47,35 +73,18 @@ for plevi in plevis
 end
 
 # Compute group G-types
-gptypes = Array{Any}(nothing,0,6);
+gptypes = Array{Any}(nothing,0,5);
 for plevi in plevis
 	uc_plevi = UnipotentCharacters(plevi);
 	name = charnames(uc_plevi,limit=true);
 	ucdegs = CycPoldegrees(uc_plevi);
 	for i in 1:length(uc_plevi)
 		if Int(ucdegs[i](1))!=0
-			global gptypes = vcat(gptypes,[plevi Int((length(roots(G)) - length(roots(plevi)))/2) orderpol(plevi) name[i] ucdegs[i] Int(ucdegs[i](1))]);
+			global gptypes = vcat(gptypes,[(plevi,name[i]) Int((length(roots(G)) - length(roots(plevi)))/2) ucdegs[i] orderpol(plevi) Int(ucdegs[i](1))]);
 		end
 	end
 end
 
-# Compute alg G-types
-algtypes = Array{Any}(nothing,0,6);
-for levi in levis
-	uc_levi = UnipotentCharacters(levi);
-	name = charnames(uc_levi,limit=true);
-	ucdegs = CycPoldegrees(uc_levi);
-	levi_size = CycPol(PermRoot.generic_order(levi,Pol(:q)))
-	for i in 1:length(uc_levi)
-		if Int(ucdegs[i](1))!=0
-			global algtypes = vcat(algtypes,[levi Int((length(roots(G)) - length(roots(levi)))/2) levi_size name[i] ucdegs[i] Int(ucdegs[i](1))]);
-		end
-	end
-end
-
-
-
-
-
-
+# Sort according to semisimple rank
+sortslices(gptypes,dims=1,by=x->x[2]);
 

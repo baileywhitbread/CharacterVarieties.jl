b565039c2e8cfe734dbbf04df026a00a05ad0a08
Bailey Whitbread <98004245+baileywhitbread@users.noreply.github.com>
2024-04-18 15:59:48 +1000

    Update paper.jl
    Rearranged to speed up calculations

--- paper.jl
+++ paper.jl
@@ -12,6 +12,10 @@ function pi0(L)
 	return length(algebraic_center(L).AZ)
 end
 
+function ssrank_diff(L)
+	Int((length(roots(G)) - length(roots(L)))/2)
+end
+
 function subset(L,M)
 	return issubset(inclusion(L),inclusion(M))
 end
@@ -32,6 +36,10 @@ function mob(A,B,poset)
 	end
 end
 
+function orbit(L)
+	return 1
+end
+
 function nu(L)
 	nuvalue = 0
 	for iplevi in iplevis
@@ -43,32 +51,31 @@ function nu(L)
 end
 
 
+
 # Choose group G
 G = rootdatum(:G2);
 
 # Gather info about G
-Gdual = rootdatum(simplecoroots(G),simpleroots(G));
-rank = size(simpleroots(G),2);
-ssrank = size(simpleroots(G),1);
+G_dual = rootdatum(simplecoroots(G),simpleroots(G));
+G_rank = size(simpleroots(G),2);
+G_ssrank = size(simpleroots(G),1);
 W = rootdatum(cartan(G));
-orderT = orderpol(torus(rank));
-orderZ = orderpol(torus(rank-ssrank));
+T_order = orderpol(torus(G_rank));
+Z_order = orderpol(torus(G_rank-G_ssrank));
 
-# Compute pseudo Levis and isolated pseudo Levis in Gdual
-plevis = reflection_subgroup.(Ref(Gdual),sscentralizer_reps(Gdual)); 
+# Compute pseudo Levis and isolated pseudo Levis in G_dual
+plevis = reflection_subgroup.(Ref(G_dual),sscentralizer_reps(G_dual)); 
 iplevis = [];
 for plevi in plevis
-	for iplevi in unique(map(L -> L.group, centralizer.(Ref(Gdual),quasi_isolated_reps(Gdual))))
+	for iplevi in unique(map(L -> L.group, centralizer.(Ref(G_dual),quasi_isolated_reps(G_dual))))
 		if sort(inclusion(plevi)) == sort(inclusion(iplevi))
 			append!(iplevis,[plevi])
 		end
 	end
 end
 
-
-
-# Compute Levis in Gdual
-levis = map(L -> L.W, split_levis(Gdual)); 
+# Compute Levis in G_dual
+levis = map(L -> L.W, split_levis(G_dual)); 
 
 # Compute number of G-types
 number_of_gptypes = 0;
@@ -83,15 +90,25 @@ end
 # Compute group G-types
 gptypes = Array{Any}(nothing,0,7);
 for plevi in plevis
+	order_plevi = orderpol(plevi);
+	ssrank_diff_plevi = ssrank_diff(plevi);
+	orbit_plevi = "???";
+	nu_plevi = nu(plevi);
 	uc_plevi = UnipotentCharacters(plevi);
 	name = charnames(uc_plevi,limit=true);
 	ucdegs = CycPoldegrees(uc_plevi);
 	for i in 1:length(uc_plevi)
 		if Int(ucdegs[i](1))!=0
-			global gptypes = vcat(gptypes,[(plevi,name[i]) Int((length(roots(G)) - length(roots(plevi)))/2) ucdegs[i] orderpol(plevi) Int(ucdegs[i](1)) 0 nu(plevi)]);
+			global gptypes = vcat(gptypes,
+			[(plevi,name[i]) ssrank_diff_plevi ucdegs[i] order_plevi Int(ucdegs[i](1)) orbit_plevi nu_plevi]
+			);
 		end
 	end
 end
 
-# Sort according to semisimple rank
+# Sort according to ssrank(G)-ssrank(L)
 sortslices(gptypes,dims=1,by=x->x[2]);
+
+#labels = ["([L],ρ)","r(G)-r(L)","|L(Fq)|","χᵨ(1)","ρ(1)","|[L]|","nu(L)"];
+#showtable(gptypes;col_labels=labels,align="|r|r|r|r|r|r|r|r|")
+

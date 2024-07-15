cdb5e6eb00ac149133261b96fe52e5be3ad0e0dc
q-bailey <baileycwhitbread@gmail.com>
2024-07-15 10:12:31 +1000

    Update CharacterVarieties.jl
    Added BigInt's in most places involving the polynomial for |Y(Fq)| in order to check non-negativity

--- src/CharacterVarieties.jl
+++ src/CharacterVarieties.jl
@@ -107,12 +107,12 @@ function islevi(L::FiniteCoxeterGroup)
 	end
 end
 
-function ispalindromic(f::Pol{Int64})
+function ispalindromic(f::Pol{BigInt})
 	# Returns true iff f palindromic
 	return ((f(0)!= 0) && (f.c == f.c[end:-1:1])) || (f(exp(1))==0)
 end
 
-function isnonnegative(f::Pol{Int64})
+function isnonnegative(f::Pol{BigInt})
 	# Returns true iff f has non-negative coefficients
 	return length(filter(x -> x<0, f.c)) == 0
 end
@@ -375,51 +375,64 @@ end
 function Htau(G::FiniteCoxeterGroup,n::Int64,i::Int64)
 	# Returns Hτ(q) = q^(n|Φ(G)+| + dim(Z)) * (|G(Fq)|/|L(Fq)|) * |N| * Q_L^T(N)^n * |[L]| * (|W|/|W(L)|)^(n-1) * µ(L,G)
 	# where τ = [L,ρ] is the ith GType and n is the number of punctures
-	G_weyl_size = length(G)
-	G_pos_root_size = Int64(length(G)/2)
-	Z_dim = rank(G)-semisimplerank(G)
+	G_weyl_size = BigInt(length(G))
+	G_pos_root_size = BigInt(length(G)/2)
+	Z_dim = BigInt(rank(G)-semisimplerank(G))
 	row_data = algebra_type_data(G)[i,:]
 	L_size = orderpol(row_data[1].levi)
 	N_size = row_data[1].size
 	L_green = row_data[1].green
 	orbit_size = row_data[5]
-	L_weyl_size = length(row_data[1].levi)
+	L_weyl_size = BigInt(length(row_data[1].levi))
 	mu_L = row_data[6]
-	return Pol{Rational{Int64}}(Pol(:q)^(n*G_pos_root_size + Z_dim) * Pol{Int64}(orderpol(G)//L_size) * N_size * L_green^n * Int64(orbit_size * (G_weyl_size/L_weyl_size)^(n-1) * mu_L))
+	return Pol{Rational{BigInt}}(Pol(:q)^(n*G_pos_root_size + Z_dim) * Pol{BigInt}(orderpol(G)//L_size) * N_size * L_green^n * BigInt(orbit_size * BigInt((G_weyl_size/L_weyl_size)^(n-1)) * mu_L))
 end
 
 function fast_qdtau(G::FiniteCoxeterGroup,i::Int64,type_data)
 	# Returns q^(d(τ)) where τ = [L,ρ] is the ith GType
-	return Pol(:q)^(type_data[i,:][2])
+	return Pol{BigInt}(Pol(:q)^(type_data[i,:][2]))
 end
 
 function fast_Htau(G::FiniteCoxeterGroup,n::Int64,i::Int64,type_data)
-	# Returns Hτ(q) = q^(n|Φ(G)+| + dim(Z)) * (|G(Fq)|/|L(Fq)|) * |N| * Q_L^T(N)^n * |[L]| * (|W|/|W(L)|)^(n-1) * µ(L,G)
+	# Returns Hτ(q) = q^(n|Φ(G)+| + dim(Z)) * (|G(Fq)|/|L(Fq)|) * |N| * Q_L^T(N)^n * |[L]| * EY(G,) * µ(L,G)
 	# where τ = [L,ρ] is the ith GType and n is the number of punctures
-	G_weyl_size = length(G)
-	G_pos_root_size = Int64(length(G)/2)
-	Z_dim = rank(G)-semisimplerank(G)
-	L_size = orderpol(type_data[i,:][1].levi)
-	N_size = type_data[i,:][1].size
-	L_green = type_data[i,:][1].green
-	orbit_size = type_data[i,:][5]
-	L_weyl_size = length(type_data[i,:][1].levi)
-	mu_L = type_data[i,:][6]
-	return Pol{Rational{Int64}}(Pol(:q)^(n*G_pos_root_size + Z_dim) * Pol{Int64}(orderpol(G)//L_size) * N_size * L_green^n * Int64(orbit_size * (G_weyl_size/L_weyl_size)^(n-1) * mu_L))
+	G_weyl_size = BigInt(length(G))
+	G_pos_root_size = BigInt(Int64(length(G)/2))
+	Z_dim = BigInt(rank(G)-semisimplerank(G))
+	L_size = Pol{BigInt}(orderpol(type_data[i,:][1].levi))
+	N_size = Pol{Rational{BigInt}}(type_data[i,:][1].size)
+	L_green = Pol{Rational{BigInt}}(type_data[i,:][1].green)
+	orbit_size = BigInt(type_data[i,:][5])
+	L_weyl_size = BigInt(length(type_data[i,:][1].levi))
+	mu_L = BigInt(type_data[i,:][6])
+	return Pol{Rational{BigInt}}(Pol(:q)^(n*G_pos_root_size + Z_dim) * Pol{BigInt}(orderpol(G)//L_size) * N_size * L_green^n * BigInt(orbit_size * BigInt((G_weyl_size/L_weyl_size)^(n-1)) * mu_L))
 end
 
 function EY(G::FiniteCoxeterGroup,g::Int64,n::Int64)
 	# Returns the E-polynomial E(Y;q) associated to the group G and a genus g surface with n punctures
 	d = algebra_type_data(G)
-	Z_size = orderpol(torus(rank(G)-semisimplerank(G)))
-	G_size = orderpol(G)
-	g_size = Pol(:q)^(degree(G_size))
+	Z_size = Pol{BigInt}(orderpol(torus(rank(G)-semisimplerank(G))))
+	G_size = Pol{BigInt}(orderpol(G))
+	g_size = Pol{Int64}(Pol(:q)^(degree(G_size)))
 
-	type_sum = 0
+	type_sum = Pol{BigInt}(0)
 	for i in 1:size(d)[1]
 		type_sum += fast_qdtau(G,i,d)^(g)*fast_Htau(G,n,i,d)
 	end
-	return Pol{Int64}( (Z_size//G_size) * g_size^(g-1) * type_sum ) 
+	return Pol{BigInt}((Z_size//G_size) * g_size^(g-1) * type_sum ) 
+end
+
+function fast_EY(G::FiniteCoxeterGroup,g::Int64,n::Int64,type_data)
+	# Returns the E-polynomial E(Y;q) associated to the group G and a genus g surface with n punctures
+	Z_size = Pol{BigInt}(orderpol(torus(rank(G)-semisimplerank(G))))
+	G_size = Pol{BigInt}(orderpol(G))
+	g_size = Pol{Int64}(Pol(:q)^(degree(G_size)))
+
+	type_sum = Pol{BigInt}(0)
+	for i in 1:size(type_data)[1]
+		type_sum += fast_qdtau(G,i,type_data)^(g)*fast_Htau(G,n,i,type_data)
+	end
+	return Pol{BigInt}((Z_size//G_size) * g_size^(g-1) * type_sum ) 
 end
 
 ###############################################################################
@@ -568,6 +581,7 @@ function palindrome_X(G,genus_max,puncture_max)
 			catch err
 				if isa(err,OverflowError)
 					println("Overflow error when g=",g," and n=",n)
+					break
 				else
 					println(err," when g=",g," and n=",n)
 				end
@@ -599,29 +613,10 @@ end
 
 function nonnegative_Y(G,genus,puncture_min,puncture_max)
 	# Checks negativity of coefficients of E(Y;q) with g=genus and n=puncture_min,2,...,puncture_max
+	d=algebra_type_data(G)
 	for n in puncture_min:puncture_max
 		try 
-			if isnonnegative(EY(G,genus,n))
-				println("EY does not have negative coefficients when g=",genus," and n=",n)
-			else
-				println("EY has negative coefficients when g=",genus," and n=",n)
-			end
-		catch err
-			if isa(err,OverflowError)
-				println("Overflow error when g=",genus," and n=",n)
-			else
-				println(err," when g=",genus," and n=",n)
-			end
-		end
-	end
-end
-
-
-function fast_nonnegative_Y(G,genus,puncture_min,puncture_max)
-	# Checks negativity of coefficients of E(Y;q) with g=genus and n=puncture_min,2,...,puncture_max
-	for n in puncture_min:puncture_max
-		try 
-			if isnonnegative(fast_EY(G,genus,n))
+			if isnonnegative(fast_EY(G,genus,n,d))
 				println("EY does not have negative coefficients when g=",genus," and n=",n)
 			else
 				println("EY has negative coefficients when g=",genus," and n=",n)

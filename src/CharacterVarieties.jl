0c77aecf1f300314d2a26adc98ff46b4124aaa83
q-bailey <baileycwhitbread@gmail.com>
2024-07-25 10:33:51 +1000

    Writing tests
    Update runtests.jl
    
    Update README.md
    
    Changed exported functions
    
    Update Project.toml
    
    Update CharacterVarieties.jl
    
    Update CharacterVarieties.jl
    
    Testing

--- src/CharacterVarieties.jl
+++ src/CharacterVarieties.jl
@@ -1,31 +1,22 @@
 module CharacterVarieties
 
+using Reexport
+@reexport using Chevie
 
-using Chevie
+
+
+
 
 # Export structs
 export GType, gType
 
-
 # Export functions
-## These are for users to use
+## These are for users
 export group_type_table, algebra_type_table
 export EX, EY
 
-## These support the above
-export plorbit_reps, plorbits, plevis
-export iplorbit_reps, iplorbits, iplevis
-export lorbit_reps, lorbits, levis
-
-export group_types, algebra_types
-export group_type_data, algebra_type_data
-
-## These are helper functions
-export isisolated, islevi, myorbit, orderpol, mobius, pi0, nu
-
 ## These are for testing
-export ispalindromic, isnonnegative
-export palindrome_X, euler_X, nonnegative_Y
+export palindrome_X, euler_X, nonnegative_Y, nonnegative_X
 export bigint_EX, bigint_EY
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using CharacterVarieties
 using Test
 
+
+
 @testset "CharacterVarieties.jl" begin
-    # Write your tests here.
+    @test EX(rootdatum(:gl,2),0,4)==Pol([1,4,1])
 end

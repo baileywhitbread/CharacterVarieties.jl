847a01d1416ed9e78d44f9e4bea0fb288b32c9dc
q-bailey <baileycwhitbread@gmail.com>
2024-07-31 09:09:23 +1000

    fast BigInt epolys

--- src/epolys.jl
+++ src/epolys.jl
@@ -71,13 +71,88 @@ end
 
 
 
+### BigInt versions for testing.jl
+function bigint_Mtau(G::FiniteCoxeterGroup,i::Int64)
+	# Returns Mτ(q) = q^(|Φ(G)+|-|Φ(L)+|) |L(Fq)|/ρ(1) 
+	# where τ = [L,ρ] is the ith GType
+	type_data = group_type_data(G)
+	L_pos_root_size = BigInt(type_data[i,:][2])
+	L_size = Pol{Rational{BigInt}}(type_data[i,:][3])
+	rho_deg = Pol{Rational{BigInt}}(type_data[i,:][4])
+	return Pol{Rational{BigInt}}(Pol(:q)^(BigInt(length(roots(G))/2)-L_pos_root_size)*Pol{Rational{BigInt}}(L_size//rho_deg))
+end
+
+function bigint_Stau(G::FiniteCoxeterGroup,n::Int64,i::Int64)
+	# Returns Sτ(q) = |Z(Fq)| * χᵨ(1)^n * |[L]| * (|W|/|W(L)|)^(n-1) * ν(L)
+	# where τ = [L,ρ] is the ith GType and n is the number of punctures
+	type_data = group_type_data(G)
+	Z_size = Pol{Rational{BigInt}}(orderpol(torus(rank(G)-semisimplerank(G))))
+	chi_rho_deg = BigInt(type_data[i,:][5])
+	G_weyl_size = BigInt(length(G))
+	L_weyl_size = BigInt(type_data[i,:][6])
+	orbit_size = BigInt(type_data[i,:][7])
+	nu_L = BigInt(type_data[i,:][8])
+	return Pol{Rational{BigInt}}(Z_size * chi_rho_deg^n * orbit_size * (BigInt(G_weyl_size//L_weyl_size))^(n-1) * BigInt(nu_L))
+end
+
+function bigint_EX(G::FiniteCoxeterGroup,g::Int64,n::Int64)
+	# Returns the E-polynomial E(X;q) associated to the group G and a genus g surface with n punctures
+	type_data = group_type_data(G)
+	Z_size = Pol{BigInt}(orderpol(torus(rank(G)-semisimplerank(G))))
+	T_size = Pol{BigInt}(orderpol(torus(rank(G))))
+	type_sum = BigInt(0)
+	for i in 1:size(type_data)[1]
+		type_sum += fast_bigint_Mtau(G,i,type_data)^(2g-2+n)*fast_bigint_Stau(G,n,i,type_data)
+	end
+	return Pol{BigInt}(Pol{Rational{BigInt}}((Z_size//T_size^n))*type_sum)
+end
+
+function bigint_qdtau(G::FiniteCoxeterGroup,i::Int64)
+	# Returns q^(d(τ)) where τ = [L,ρ] is the ith GType
+	type_data = group_type_data(G)
+	return Pol{BigInt}(Pol(:q)^(type_data[i,:][2]))
+end
+
+function bigint_Htau(G::FiniteCoxeterGroup,n::Int64,i::Int64)
+	# Returns Hτ(q) = q^(n|Φ(G)+| + dim(Z)) * (|G(Fq)|/|L(Fq)|) * |N| * Q_L^T(N)^n * |[L]| * (|W|/|W(L)|)^(n-1) * µ(L,G)
+	# where τ = [L,ρ] is the ith GType and n is the number of punctures
+	type_data = group_type_data(G)
+	G_weyl_size = BigInt(length(G))
+	G_pos_root_size = BigInt(length(G)/2)
+	Z_dim = BigInt(rank(G)-semisimplerank(G))
+	L_size = Pol{Rational{BigInt}}(orderpol(type_data[i,:][1].levi))
+	N_size = Pol{Rational{BigInt}}(type_data[i,:][1].size)
+	L_green = Pol{Rational{BigInt}}(type_data[i,:][1].green)
+	orbit_size = BigInt(type_data[i,:][5])
+	L_weyl_size = BigInt(length(type_data[i,:][1].levi))
+	mu_L = BigInt(type_data[i,:][6])
+	return Pol(:q)^(BigInt(n*G_pos_root_size + Z_dim)) * (orderpol(G)//L_size) * N_size * L_green^n * BigInt(orbit_size) * BigInt((BigInt(G_weyl_size)/BigInt(L_weyl_size))^(n-1)) * BigInt(mu_L)
+end
+
+
+
+function bigint_EY(G::FiniteCoxeterGroup,g::Int64,n::Int64)
+	# Returns the E-polynomial E(Y;q) associated to the group G and a genus g surface with n punctures
+	type_data = group_type_data(G)
+	Z_size = Pol{Rational{BigInt}}(orderpol(torus(rank(G)-semisimplerank(G))))
+	G_size = Pol{Rational{BigInt}}(orderpol(G))
+	g_size = Pol{Rational{BigInt}}(Pol(:q)^(BigInt(degree(G_size))))
+
+	type_sum = Pol{BigInt}(0)
+	for i in 1:size(type_data)[1]
+		type_sum += fast_bigint_qdtau(G,i,type_data)^(g)*fast_bigint_Htau(G,n,i,type_data)
+	end
+	return Pol{BigInt}((Z_size//G_size) * (g_size^(g)//g_size) * type_sum)
+end
 
 
 
 
 
-### BigInt version for testing.jl
-function bigint_Mtau(G::FiniteCoxeterGroup,i::Int64,type_data)
+
+
+### "Fast" BigInt versions for testing.jl
+function fast_bigint_Mtau(G::FiniteCoxeterGroup,i::Int64,type_data)
 	# Returns Mτ(q) = q^(|Φ(G)+|-|Φ(L)+|) |L(Fq)|/ρ(1) 
 	# where τ = [L,ρ] is the ith GType
 	L_pos_root_size = BigInt(type_data[i,:][2])
@@ -86,7 +161,7 @@ function bigint_Mtau(G::FiniteCoxeterGroup,i::Int64,type_data)
 	return Pol{Rational{BigInt}}(Pol(:q)^(BigInt(length(roots(G))/2)-L_pos_root_size)*Pol{Rational{BigInt}}(L_size//rho_deg))
 end
 
-function bigint_Stau(G::FiniteCoxeterGroup,n::Int64,i::Int64,type_data)
+function fast_bigint_Stau(G::FiniteCoxeterGroup,n::Int64,i::Int64,type_data)
 	# Returns Sτ(q) = |Z(Fq)| * χᵨ(1)^n * |[L]| * (|W|/|W(L)|)^(n-1) * ν(L)
 	# where τ = [L,ρ] is the ith GType and n is the number of punctures
 	Z_size = Pol{Rational{BigInt}}(orderpol(torus(rank(G)-semisimplerank(G))))
@@ -98,23 +173,23 @@ function bigint_Stau(G::FiniteCoxeterGroup,n::Int64,i::Int64,type_data)
 	return Pol{Rational{BigInt}}(Z_size * chi_rho_deg^n * orbit_size * (BigInt(G_weyl_size//L_weyl_size))^(n-1) * BigInt(nu_L))
 end
 
-function bigint_EX(G::FiniteCoxeterGroup,g::Int64,n::Int64,type_data)
+function fast_bigint_EX(G::FiniteCoxeterGroup,g::Int64,n::Int64,type_data)
 	# Returns the E-polynomial E(X;q) associated to the group G and a genus g surface with n punctures
 	Z_size = Pol{BigInt}(orderpol(torus(rank(G)-semisimplerank(G))))
 	T_size = Pol{BigInt}(orderpol(torus(rank(G))))
 	type_sum = BigInt(0)
 	for i in 1:size(type_data)[1]
-		type_sum += bigint_Mtau(G,i,type_data)^(2g-2+n)*bigint_Stau(G,n,i,type_data)
+		type_sum += fast_bigint_Mtau(G,i,type_data)^(2g-2+n)*fast_bigint_Stau(G,n,i,type_data)
 	end
 	return Pol{BigInt}(Pol{Rational{BigInt}}((Z_size//T_size^n))*type_sum)
 end
 
-function bigint_qdtau(G::FiniteCoxeterGroup,i::Int64,type_data)
+function fast_bigint_qdtau(G::FiniteCoxeterGroup,i::Int64,type_data)
 	# Returns q^(d(τ)) where τ = [L,ρ] is the ith GType
 	return Pol{BigInt}(Pol(:q)^(type_data[i,:][2]))
 end
 
-function bigint_Htau(G::FiniteCoxeterGroup,n::Int64,i::Int64,type_data)
+function fast_bigint_Htau(G::FiniteCoxeterGroup,n::Int64,i::Int64,type_data)
 	# Returns Hτ(q) = q^(n|Φ(G)+| + dim(Z)) * (|G(Fq)|/|L(Fq)|) * |N| * Q_L^T(N)^n * |[L]| * (|W|/|W(L)|)^(n-1) * µ(L,G)
 	# where τ = [L,ρ] is the ith GType and n is the number of punctures
 	G_weyl_size = BigInt(length(G))
@@ -131,7 +206,7 @@ end
 
 
 
-function bigint_EY(G::FiniteCoxeterGroup,g::Int64,n::Int64,type_data)
+function fast_bigint_EY(G::FiniteCoxeterGroup,g::Int64,n::Int64,type_data)
 	# Returns the E-polynomial E(Y;q) associated to the group G and a genus g surface with n punctures
 	Z_size = Pol{Rational{BigInt}}(orderpol(torus(rank(G)-semisimplerank(G))))
 	G_size = Pol{Rational{BigInt}}(orderpol(G))
@@ -139,7 +214,7 @@ function bigint_EY(G::FiniteCoxeterGroup,g::Int64,n::Int64,type_data)
 
 	type_sum = Pol{BigInt}(0)
 	for i in 1:size(type_data)[1]
-		type_sum += bigint_qdtau(G,i,type_data)^(g)*bigint_Htau(G,n,i,type_data)
+		type_sum += fast_bigint_qdtau(G,i,type_data)^(g)*fast_bigint_Htau(G,n,i,type_data)
 	end
 	return Pol{BigInt}((Z_size//G_size) * (g_size^(g)//g_size) * type_sum)
 end
--- src/testing.jl
+++ src/testing.jl
@@ -33,10 +33,10 @@ function euler_X(G,genus,puncture_min,puncture_max)
 	for n in puncture_min:puncture_max
 		try 
 			print("Computing χ(X) when g=",genus," and n=",n,": ")
-			if fast_EX(G,genus,n,d)(1)==0
+			if fast_bigint_EX(G,genus,n,d)(1)==0
 				println("χ(X)=0")
 			else
-				println("χ(X)=",bigint_EX(G,genus,n,d)(1))
+				println("χ(X)=",fast_bigint_EX(G,genus,n,d)(1))
 			end
 		catch err
 			if isa(err,OverflowError)
@@ -59,7 +59,7 @@ function nonnegative_Y(G,genus,puncture_min,puncture_max)
 	for n in puncture_min:puncture_max
 		try 
 			print("Checking coefficients of E(Y;q) when g=",genus," and n=",n,": ")
-			if isnonnegative(bigint_EY(G,genus,n,d))
+			if isnonnegative(fast_bigint_EY(G,genus,n,d))
 				println("All non-negative")
 			else
 				println("Negative coefficients found")
@@ -86,7 +86,7 @@ function nonnegative_X(G,genus,puncture_min,puncture_max)
 	for n in puncture_min:puncture_max
 		try 
 			print("Checking coefficients of E(X;q) when g=",genus," and n=",n,": ")
-			if isnonnegative(bigint_EX(G,genus,n,d))
+			if isnonnegative(fast_bigint_EX(G,genus,n,d))
 				println("All non-negative")
 			else
 				println("Negative coefficients found")

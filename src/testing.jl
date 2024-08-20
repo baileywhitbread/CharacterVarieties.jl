4a64bf6d85a68e5e8ac1dcbc8e828b9ddf9056f7
q-bailey <baileycwhitbread@gmail.com>
2024-08-20 14:22:35 +1000

    Update testing.jl

--- src/testing.jl
+++ src/testing.jl
@@ -80,6 +80,8 @@ function nonnegative_Y(G::FiniteCoxeterGroup,genus::Int64,puncture_min::Int64,pu
 end
 
 
+
+
 function nonnegative_X(G::FiniteCoxeterGroup,genus::Int64,puncture_min::Int64,puncture_max::Int64)
 	# Checks negativity of coefficients of E(X;q) with g=genus and n=puncture_min,...,puncture_max
 	d=group_type_data(G)
@@ -90,6 +92,38 @@ function nonnegative_X(G::FiniteCoxeterGroup,genus::Int64,puncture_min::Int64,pu
 				println("All non-negative")
 			else
 				println("Negative coefficients found")
+			end
+		catch err
+			if isa(err,OverflowError)
+				println("Overflow error")
+				#break
+			elseif isa(err,ErrorException)
+				println("Error exception")
+				#break
+			else
+				println(err)
+				#break
+			end
+		end
+	end
+end
+
+
+
+function log_nonnegative_Y(G::FiniteCoxeterGroup,genus::Int64,puncture_min::Int64,puncture_max::Int64)
+	# Checks negativity of coefficients of E(Y;q) with g=genus and n=puncture_min,...,puncture_max
+	rand_string = randstring(12)
+	log_name = "EY_coefficients_G_"*string(G)*"_"*rand_string*".txt"
+	io = open(log_name, "w+")
+	global_logger(SimpleLogger(io))
+	d=algebra_type_data(G)
+	for n in puncture_min:puncture_max
+		try 
+			@info("Checking coefficients of E(Y;q) when G=",G,", g=",genus," and n=",n,": ")
+			if isnonnegative(fast_bigint_EY(G,genus,n,d))
+				@info("All non-negative")
+			else
+				@info("Negative coefficients found")
 			end
 		catch err
 			if isa(err,OverflowError)

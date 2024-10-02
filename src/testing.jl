f2c1343bf244e6e10fda734db8b82d5ffa6e15e0
q-bailey <baileycwhitbread@gmail.com>
2024-10-02 11:44:34 +1000

    Update testing.jl

--- src/testing.jl
+++ src/testing.jl
@@ -1,11 +1,11 @@
 ## Testing E-polynomials
-function palindrome_X(G::FiniteCoxeterGroup,genus,puncture_min,puncture_max)
+function palindrome_X(G::FiniteCoxeterGroup,g,puncture_min,puncture_max)
 	# Checks palindromicity of X with g=genus and n=puncture_min,...,puncture_max
 	d=group_type_data(G)
 	for n in puncture_min:puncture_max
 		try 
-			print("Checking E(X;q) palindromic when G=$G, g=$genus and n=$n: ")
-			if ispalindromic(fast_EX(G,genus,n,d))
+			print("Checking E(X;q) palindromic when (G,g,n)=($G,$g,$n): ")
+			if ispalindromic(fast_EX(G,g,n,d))
 				println("Yes")
 			else
 				println("No")
@@ -18,16 +18,16 @@ end
 
 
 
-function euler_X(G::FiniteCoxeterGroup,genus,puncture_min,puncture_max)
+function euler_X(G::FiniteCoxeterGroup,g,puncture_min,puncture_max)
 	# Checks Euler characteristic of X with g=genus and n=puncture_min,...,puncture_max
 	d=group_type_data(G)
 	for n in puncture_min:puncture_max
 		try 
-			print("Computing χ(X) when G=$G, g=$genus and n=$n: ")
-			if fast_EX(G,genus,n,d)(1)==0
+			print("If (G,g,n)=($G,$g,$n) then ")
+			if fast_EX(G,g,n,d)(1)==0
 				println("χ(X)=0")
 			else
-				println("χ(X)=",fast_EX(G,genus,n,d)(1))
+				println("χ(X)=",fast_EX(G,g,n,d)(1))
 			end
 		catch err 
 			println("$err")
@@ -35,13 +35,13 @@ function euler_X(G::FiniteCoxeterGroup,genus,puncture_min,puncture_max)
 	end
 end
 
-function nonnegative_Y(G::FiniteCoxeterGroup,genus,puncture_min,puncture_max)
+function nonnegative_Y(G::FiniteCoxeterGroup,g,puncture_min,puncture_max)
 	# Checks negativity of coefficients of E(Y;q) with g=genus and n=puncture_min,...,puncture_max
 	d=algebra_type_data(G)
 	for n in puncture_min:puncture_max
 		try 
-			print("Checking coefficients of E(X;q) when G=$G, g=$genus and n=$n: ")
-			if isnonnegative(fast_EY(G,genus,n,d))
+			print("Checking coefficients of E(X;q) when (G,g,n)=($G,$g,$n): ")
+			if isnonnegative(fast_EY(G,g,n,d))
 				println("All non-negative")
 			else
 				println("Negative coefficients found")
@@ -55,13 +55,13 @@ end
 
 
 
-function nonnegative_X(G::FiniteCoxeterGroup,genus,puncture_min,puncture_max)
+function nonnegative_X(G::FiniteCoxeterGroup,g,puncture_min,puncture_max)
 	# Checks negativity of coefficients of E(X;q) with g=genus and n=puncture_min,...,puncture_max
 	d=group_type_data(G)
 	for n in puncture_min:puncture_max
 		try 
-			print("Checking coefficients of E(X;q) when G=$G, g=$genus and n=$n: ")
-			if isnonnegative(fast_EX(G,genus,n,d))
+			print("Checking coefficients of E(X;q) when (G,g,n)=($G,$g,$n): ")
+			if isnonnegative(fast_EX(G,g,n,d))
 				println("All non-negative")
 			else
 				println("Negative coefficients found")
@@ -74,19 +74,19 @@ end
 
 
 
-function log_nonnegative_Y(G::FiniteCoxeterGroup,genus,puncture_min,puncture_max)
+function log_nonnegative_Y(G::FiniteCoxeterGroup,g,puncture_min,puncture_max)
 	# Checks negativity of coefficients of E(Y;q) with g=genus and n=puncture_min,...,puncture_max
-	log_name = "EY_nonnegative_coeff_"*xrepr(rio(),G)*"_g="*string(genus)*"_n="*string(puncture_min)*"..."*string(puncture_max)*"_"*randstring(12)*".txt"
+	log_name = "EY_nonnegative_coeff_"*xrepr(rio(),G)*"_g="*string(g)*"_n="*string(puncture_min)*"..."*string(puncture_max)*"_"*randstring(12)*".txt"
 	io = open(log_name, "w+")
 	logger = SimpleLogger(io)
 	global_logger(logger)
 	d=algebra_type_data(G)
 	for n in puncture_min:puncture_max
 		try 
-			if isnonnegative(fast_EY(G,genus,n,d))
-				@info("Coefficients all non-negative when n=$n")
+			if isnonnegative(fast_EY(G,g,n,d))
+				@info("Coefficients all non-negative when (G,g,n)=($G,$g,$n)")
 			else
-				@info("Negative coefficients found when n=$n")
+				@info("Negative coefficients found when (G,g,n)=($G,$g,$n)")
 			end
 		catch err 
 			println("$err")
@@ -96,13 +96,12 @@ function log_nonnegative_Y(G::FiniteCoxeterGroup,genus,puncture_min,puncture_max
 end
 
 function check_dim_X(G::FiniteCoxeterGroup,genus_min,genus_max,puncture_min,puncture_max)
-	d=group_type_data(G)
 	for n in puncture_min:puncture_max
 		for g in genus_min:genus_max
 			if dimension_XY(G,g,n) == degree(fast_EX(G,g,n,d))
-				println("Dimension correct when (g,n) is ($g,$n)")
+				println("Dimension correct when (G,g,n)=($G,$g,$n)")
 			else
-				println("Dimension wrong when (g,n) is ($g,$n)")
+				println("Dimension wrong when (G,g,n)=($G,$g,$n)")
 			end
 		end
 	end
@@ -113,9 +112,9 @@ function check_dim_Y(G::FiniteCoxeterGroup,genus_min,genus_max,puncture_min,punc
 	for n in puncture_min:puncture_max
 		for g in genus_min:genus_max
 			if dimension_XY(G,g,n) == degree(fast_EY(G,g,n,d))
-				println("Dimension correct when (g,n) is ($g,$n)")
+				println("Dimension correct when (G,g,n)=($G,$g,$n)")
 			else
-				println("Dimension wrong when (g,n) is ($g,$n)")
+				println("Dimension wrong when (G,g,n)=($G,$g,$n)")
 			end
 		end
 	end

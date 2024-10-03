eb2bf5b1f696bbddbb5e97ec78745c765e299f40
q-bailey <baileycwhitbread@gmail.com>
2024-10-03 10:45:00 +1000

    Revert "Testing Claude.ai"
    This reverts commit 3ff499601f353c9f350c7e69e48928b3858fe030.

--- src/CharacterVarieties.jl
+++ src/CharacterVarieties.jl
@@ -1,38 +1,62 @@
 module CharacterVarieties
 
 using Reexport
-@reexport using Chevie, Logging, Random
+@reexport using Chevie
+@reexport using Logging
+@reexport using Random
+
+
+
+
+
 
 # Export structs
 export GType, gType
 
-# Export user functions
+# Export functions
+## These are for users
 export group_type_table, algebra_type_table
 export group_types, algebra_types
-export EX, EY, ESp4
-export plorbit_reps, plorbits, plevis
-export iplorbit_reps, iplorbits, iplevis
-export lorbit_reps, lorbits, levis
+export EX, EY
+export ESp4
 
-# Export functions for testing
+export plorbit_reps
+export plorbits
+export plevis
+
+export iplorbit_reps
+export iplorbits
+export iplevis
+
+export lorbit_reps
+export lorbits
+export levis
+
+## These are for testing
 export palindrome_X, euler_X, nonnegative_Y, nonnegative_X
 export log_nonnegative_Y
 export check_dim_X, check_dim_Y
 
-# These may eventually stop being exported
+## Eventually stop exporting these
 export dimension_XY
 export group_type_data
 export algebra_type_data
 
-# Include additional files
+
+
 include("checks.jl")
 include("helpers.jl")
 include("plevis.jl")
+
 include("grouptypes.jl")
 include("algebratypes.jl")
+
 include("epolys.jl")
 include("cambo.jl")
 include("tables.jl")
+
 include("testing.jl")
+
+
 
 end # End of module CharacterVarieties

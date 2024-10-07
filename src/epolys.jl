462591d544311f12de8095343d172d361315e304
Bailey Whitbread <baileycwhitbread@gmail.com>
2024-10-08 06:15:25 +1000

    Update epolys.jl

--- src/epolys.jl
+++ src/epolys.jl
@@ -44,7 +44,24 @@ function fast_EX(G::FiniteCoxeterGroup,g::Union{BigInt,Integer},n::Union{BigInt,
 end
 
 
-
+function new_EX(G::FiniteCoxeterGroup,g::Union{BigInt,Integer},n::Union{BigInt,Integer})
+	type_data = group_type_data(G)
+	sum = Pol{BigInt}(0)
+	for i in 1:size(type_data)[1]
+		row = type_data[i,:] # type_data[i,:] is the ith row
+		term = Pol{BigInt}(1)
+		term *= Pol(:q)^(BigInt(length(roots(G))/2-length(roots(row[1].endoscopy))/2)) # q^(|Phi(G)+|-|Phi(L)+|)
+		term *= Pol{Rational{BigInt}}(orderpol(row[1].endoscopy)//row[1].degree) # |L(Fq)|/rho(q)
+		term *= (Pol(:q)-1)^BigInt(rank(G)-semisimplerank(G)) # |Z(Fq)|
+		term *= BigInt(row[5])^BigInt(n) # dim(rho)^n
+		term *= BigInt(length(G)//row[6])^BigInt(n-1) # (|W|/|W(L)|)^(n-1)
+		term *= row[7] # |[L]|
+		term *= row[8] # nu(L)
+		sum += term
+	end
+	sum *= (Pol(:q)-1)^BigInt(rank(G)-semisimplerank(G))//((sizeofT)^BigInt(n)) # |Z(Fq)|/|T(Fq)|^n
+	return Pol{BigInt}(sum)
+end
 
 
 
@@ -52,23 +69,21 @@ end
 
 function fast_qdtau(G::FiniteCoxeterGroup,i::Union{BigInt,Integer},type_data::Any)
 	# Returns q^(d(τ)) where τ = [L,ρ] is the ith GType
-	return Pol{BigInt}(Pol(:q)^(type_data[i,:][2]))
+	row = type_data[i,:] # type_data[i,:] is the ith row
+	return Pol{BigInt}(Pol(:q)^(BigInt(row[2])))
 end
 
 function fast_Htau(G::FiniteCoxeterGroup,n::Union{BigInt,Integer},i::Union{BigInt,Integer},type_data::Any)
-	# Returns Hτ(q) = q^(n|Φ(G)+| + dim(Z)) * (|G(Fq)|/|L(Fq)|) * |N| * Q_L^T(N)^n * |[L]| * (|W|/|W(L)|)^(n-1) * µ(L,G)
-	# where τ = [L,ρ] is the ith GType and n is the number of punctures
-	n =  BigInt(n)
-	G_weyl_size = BigInt(length(G))
-	G_pos_root_size = BigInt(length(roots(G))/2)
-	Z_dim = BigInt(rank(G)-semisimplerank(G))
-	L_size = Pol{Rational{BigInt}}(orderpol(type_data[i,:][1].levi))
-	N_size = Pol{Rational{BigInt}}(type_data[i,:][1].size)
-	L_green = Pol{Rational{BigInt}}(type_data[i,:][1].green)
-	orbit_size = BigInt(type_data[i,:][5])
-	L_weyl_size = BigInt(length(type_data[i,:][1].levi))
-	mu_L = BigInt(type_data[i,:][6])
-	return Pol(:q)^(BigInt(BigInt(n)*G_pos_root_size + Z_dim)) * (orderpol(G)//L_size) * N_size * (L_green)^BigInt(n) * BigInt(orbit_size) * BigInt((BigInt(G_weyl_size)//BigInt(L_weyl_size))^BigInt(n-1)) * BigInt(mu_L)
+	row = type_data[i,:] # type_data[i,:] is the ith row
+	term = Pol{BigInt}(1)
+	term *= Pol(:q)^(BigInt(n*(length(roots(G))/2)+(rank(G)-semisimplerank(G))))
+	term *= Frac{Pol{BigInt}}(orderpol(G)//orderpol(row[1].levi))
+	term *= Pol{Rational{BigInt}}(row[1].size)
+	term *= (Pol{BigInt}(row[1].green))^BigInt(n)
+	term *= (BigInt(BigInt(length(G))//BigInt(length(row[1].levi))))^BigInt(n-1)
+	term *= BigInt(length(myorbit(row[1].levi)))
+	term *= BigInt(mobius(row[1].levi,row[1].levi.parent,levis(G)))
+	return Frac{Pol{Rational{BigInt}}}(term)
 end
 
 function EY(G::FiniteCoxeterGroup,g::Union{BigInt,Integer},n::Union{BigInt,Integer})
@@ -88,7 +103,7 @@ function EY(G::FiniteCoxeterGroup,g::Union{BigInt,Integer},n::Union{BigInt,Integ
 		term *= BigInt(mobius(row[1].levi,row[1].levi.parent,levis(G)))
 		sum += term
 	end
-	sum *= (((Pol(:q)-1)^(rank(G)-semisimplerank(G)))//(orderpol(G)))
+	sum *= (((Pol(:q)-1)^BigInt(rank(G)-semisimplerank(G)))//(orderpol(G)))
 	sum *= ((Pol(:q)^(BigInt(g)*BigInt(degree(orderpol(G)))))//(Pol(:q)^(BigInt(1)*BigInt(degree(orderpol(G))))))
 	return Pol{BigInt}(sum)
 end

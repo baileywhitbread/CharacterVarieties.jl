8dbd4ee9d12bc56702d66f6e10978f218d25eea2
Bailey Whitbread <baileycwhitbread@gmail.com>
2025-08-08 14:59:40 +1000

    Update logging.jl

--- src/logging.jl
+++ src/logging.jl
@@ -65,3 +65,25 @@ function fast_log_nonnegative_Y(G::FiniteCoxeterGroup,g::Union{BigInt,Integer},p
 	@info(log_text)
 	close(io)
 end
+
+function fast_log_nonnegative_X(G::FiniteCoxeterGroup,g::Union{BigInt,Integer},puncture_min::Union{BigInt,Integer},puncture_max::Union{BigInt,Integer},type_data::Any)
+	# Checks negativity of coefficients of E(Y;q) with g=genus and n=puncture_min,...,puncture_max
+	log_name = "EX_nonnegative_coeff_"*xrepr(rio(),G)*"_g="*string(g)*"_n="*string(puncture_min)*"..."*string(puncture_max)*"_"*randstring(12)*".txt"
+	io = open(log_name, "w+")
+	logger = SimpleLogger(io)
+	global_logger(logger)
+	log_text = ""
+	for n in puncture_min:puncture_max
+		try 
+			if isnonnegative(fast_EX(G,g,n,type_data))
+				log_text *= "\nCoeffs of EX all non-neg when (G,g,n)=("*xrepr(rio(),G)*",$g,$n)"
+			else
+				log_text *= "\nNegative coeffs found when (G,g,n)=("*xrepr(rio(),G)*",$g,$n)"
+			end
+		catch err 
+			println("$err")
+		end
+	end
+	@info(log_text)
+	close(io)
+end
